f88d0d036eb4064d9952ca38895b911456309d55
Ollie Freeman <oliver.freeman@ndm.ox.ac.uk>
2017-11-16 11:28:21 +0000

    Add test to prove issue 1025

--- src/integration-test/groovy/rest/api/app/NonDomainServiceSpec.groovy
+++ src/integration-test/groovy/rest/api/app/NonDomainServiceSpec.groovy
@@ -1,7 +1,10 @@
 package rest.api.app
 
+import grails.core.ArtefactInfo
+import grails.core.GrailsApplication
 import grails.testing.mixin.integration.Integration
 import grails.gorm.transactions.Rollback
+import org.springframework.context.ApplicationContext
 import spock.lang.Specification
 
 @Integration
@@ -9,6 +12,21 @@ import spock.lang.Specification
 class NonDomainServiceSpec extends Specification {
 
     NonDomainService nonDomainService
+    GrailsApplication grailsApplication
+
+    void 'test artefact creation'(){
+        given:
+        ArtefactInfo artefactInfo = grailsApplication.getArtefactInfo('Service')
+        ApplicationContext applicationContext = grailsApplication.parentContext
+
+        expect:
+        applicationContext.getBean('alphaChildService')
+        applicationContext.getBean('alphaParentService')
+        applicationContext.getBean('betaChildService')
+        applicationContext.getBean('betaParentService')
+        applicationContext.getBean('supremeParentService')
+        artefactInfo.grailsClassesByName.size() == 7
+    }
 
     void 'test service binding'(){
 
